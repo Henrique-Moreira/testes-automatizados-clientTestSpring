700437a25f99d4f76cd092a32b9407acdf2305c1
Henrique Moreira <henriquemoreiraa@gmail.com>
2021-07-01 16:58:08 -0300

    start tests integration tests

--- src/test/java/com/iftm/client/tests/integration/ClientServiceIT.java
+++ src/test/java/com/iftm/client/tests/integration/ClientServiceIT.java
@@ -12,7 +12,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.iftm.client.dto.ClientDTO;
-import com.iftm.client.entities.Client;
+import com.iftm.client.repositories.ClientRepository;
 import com.iftm.client.services.ClientService;
 import com.iftm.client.services.exceptions.ResourceNotFoundException;
 
@@ -22,12 +22,17 @@ public class ClientServiceIT {
 
 	@Autowired // fazer inj de dependencia
 	private ClientService service;
+	
+	@Autowired
+	private ClientRepository repository;
 
 	private long existingId;
 	private long nonExistingId;
 	private long countTotalClients;
 	private int countClientByIncome;
 	private PageRequest pageResquest;
+	private long existingId2;
+	private String existingName, existingCpf;
 	
 	@BeforeEach
 	void setUp() throws Exception {
@@ -36,6 +41,9 @@ public class ClientServiceIT {
 		countClientByIncome = 5;
 		pageResquest = PageRequest.of(0, 10);
 		countTotalClients = 12L;
+		existingId2 = 11L;
+		existingName = "Silvio Almeida";
+		existingCpf = "10164334861";
 	}
 	
 	@Test
@@ -67,5 +75,30 @@ public class ClientServiceIT {
 		List<ClientDTO> result = service.findAll();
 		
 		Assertions.assertEquals(countTotalClients, result.size());
+	}
+	
+	// Atividade: testes de integração
+	
+	/* Implementar um teste que ao receber um id existente deve excluir o cliente com o
+	 código do id e verificar se realmente decrementou o número de clientes incluídos na
+	 base de dados.*/
+	@Test
+	public void deleteClientByIdShouldDecreaseTotalClients() {
+		service.delete(existingId);
+		
+		long totalAfterDelete = repository.count();
+		
+		Assertions.assertEquals(countTotalClients-1, totalAfterDelete);
+	}
+	
+	/*Implementar um teste que deverá testar o findById. Para o teste, você terá que
+	entrar com o código de um cliente existente e verificar se o nome e CPF do cliente
+	são correspondentes.*/
+	@Test
+	public void findByIdShouldReturnCorrectCPFAndName() {
+		ClientDTO entity = service.findById(existingId2);
+	
+		Assertions.assertTrue(existingName.equals(entity.getName()));
+		Assertions.assertEquals(existingCpf, entity.getCpf());
 	}
 }
